1fcd8a5edb18bc6e93861fed56b3edeba3b9dd73
John Kelley <johnkelley4477@gmail.com>
2018-02-19 13:32:29 -0500

    Ugly but functional

--- MongoBD/create_collection.js
+++ MongoBD/create_collection.js
@@ -3,7 +3,7 @@ var url = "mongodb://192.168.1.23:12001/workoutsDB";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  db.createCollection("workouts", function(err, res) {
+  db.createCollection("exercisetracker", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
     db.close();
