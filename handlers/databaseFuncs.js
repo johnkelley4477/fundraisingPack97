8fa61cb90d8c90c0fc1c55fc06977804fa87adc1
John Kelley <johnkelley4477@gmail.com>
2018-02-20 12:47:33 -0500

    Make data normilizer

--- handlers/databaseFuncs.js
+++ handlers/databaseFuncs.js
@@ -3,6 +3,21 @@
 const MongoClient = require('mongodb').MongoClient,
 	ObjectId = require('mongodb').ObjectId,
 	url = "mongodb://192.168.1.23:12001/workoutsDB",
+	killMeToo = [
+		{"exercise":"pullups","date":"2018-02-01","reps":"8","time":"1"},
+		{"exercise":"pullups","date":"2018-02-02","reps":"8","time":"1"},
+		{"exercise":"pullups","date":"2018-02-03","reps":"9","time":"1"},
+		{"exercise":"pushups","date":"2018-02-01","reps":"28","time":"1"},
+		{"exercise":"pushups","date":"2018-02-02","reps":"28","time":"1"},
+		{"exercise":"pushups","date":"2018-02-03","reps":"29","time":"1"},
+		{"exercise":"planks","date":"2018-02-01","reps":"1","time":"1.25"},
+		{"exercise":"planks","date":"2018-02-02","reps":"1","time":"1.25"},
+		{"exercise":"planks","date":"2018-02-03","reps":"1","time":"1.27"},
+		{"exercise":"planks","date":"2018-02-04","reps":"1","time":"1.37"},
+		{"exercise":"situps","date":"2018-02-01","reps":"28","time":"1"},
+		{"exercise":"situps","date":"2018-02-02","reps":"28","time":"1"},
+		{"exercise":"situps","date":"2018-02-03","reps":"29","time":"1"}
+	],
 	kill = {"exercises":[
 	{"exercise":"pullups",
 	"data":[
@@ -32,7 +47,8 @@ const MongoClient = require('mongodb').MongoClient,
 
 const databaseFuncs = {
 	postWorkoutData: (data,callback)=>{
-		callback(null,kill);
+		const killdata = databaseFuncs.normalizeResponce(killMeToo);
+		callback(null,killdata);
 		// MongoClient.connect(url, function(err, db) {
 		//   if (err) throw err;
 
@@ -45,6 +61,32 @@ const databaseFuncs = {
 		//     db.close();
 		//   });
 		// });
+	},
+	normalizeResponce: (data)=>{
+		let exercises = {"exercises":[]};
+		let currentExercise = "";
+		let exercise = {};
+		let dat = [];
+		data.forEach((value)=>{
+			if (currentExercise !== value.exercise){
+				if(currentExercise !== ""){
+					exercise["data"] = dat;
+					exercises.exercises.push(exercise);
+					exercise = {};
+					dat = [];
+				}
+				exercise["exercise"] = value.exercise;
+				currentExercise = value.exercise;
+			}
+			var dataObj = {};
+			dataObj["date"] = value.date;
+			dataObj["reps"] = value.reps;
+			dataObj["time"] = value.time;
+			dat.push(dataObj);
+		});
+		exercise["data"] = dat;
+		exercises.exercises.push(exercise);
+		return exercises;
 	}
 }
 
