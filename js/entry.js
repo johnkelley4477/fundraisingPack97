da15ecfa77eb063ca99f074f9ca7333678cbf148
John Kelley <johnkelley4477@gmail.com>
2018-02-21 08:19:29 -0500

    Add progress button to the entry page

--- js/entry.js
+++ js/entry.js
@@ -2,6 +2,12 @@ const entry = {
 	"today":()=>{
 		const today = new Date();
 		$('#date').val(today.getFullYear()  + '-' +  ("0" + (today.getMonth() + 1)).slice(-2) + '-' + ("0" + today.getDate()).slice(-2));
-	}
+	},
+	"mapProgressButton":$("#progress").on('click',()=>{
+		window.location.replace("/exercisetracker/progress");
+	})
 }
-$(document).ready(entry.today());
+$(document).ready(()=>{
+	entry.today()
+	entry.mapProgressButton;
+});
