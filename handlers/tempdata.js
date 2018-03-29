f0b55b91a29bbb786576e46520ecf22139d57937
John Kelley <johnkelley4477@gmail.com>
2018-03-29 09:49:27 -0400

    Add front end and UI functionaliy

--- handlers/tempdata.js
+++ handlers/tempdata.js
@@ -1,1 +1,14 @@
 'use strict'
+const data = {
+	"items" : [
+		{"id":"00000001","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"},
+		{"id":"00000002","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"},
+		{"id":"00000003","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"},
+		{"id":"00000004","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"},
+		{"id":"00000005","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"},
+		{"id":"00000006","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"},
+		{"id":"00000007","name":"Choc Crunch","desc":"stuff","cost":"5.00","price":"10.00"}
+	]
+}
+
+exports.data = data;
